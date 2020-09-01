e2f753f88c54a337eea04b6107e450cff54e1c88
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-09-01 11:32:48 -0600

    added error handling to the single.js file.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {    
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"
@@ -13,7 +26,7 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("there was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -61,4 +74,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+
+getRepoName();
